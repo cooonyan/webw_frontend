74f3f0bcd4dfed0c18051c1b04bb8465e771a362
Jaeyoung Ryu <gomtangmoru@mueslicorp.com>
2024-08-23 07:45:19 +0900

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -69,19 +69,3 @@ function Home() {
       }
 
 export default Home;
-//
-// <div>
-//   {isLoggedIn ? (
-//       <div>
-//         <h1>로그인 상태</h1>
-//         <button onClick={handleLogout}>로그아웃</button>
-//         <button onClick={() => navigate('/edit')}>편집 페이지로 이동</button>
-//       </div>
-//   ) : (
-//       <div>
-//         <h1>Guest 상태</h1>
-//         <button onClick={() => navigate('/register')}>회원가입</button>
-//         <button onClick={() => navigate('/loginform')}>로그인 하기</button>
-//       </div>
-//   )}
-// </div>
